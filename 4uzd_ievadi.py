0c97459772a78ec38903079ebfca2db6df9204cc
MaiklRG <maikls.reinsons-guza@grobinasskola.lv>
2024-01-18 09:05:34 +0200

    Init commit

---
+++ 4uzd_ievadi.py
@@ -0,0 +1,6 @@
+"""
+ Izveidot Python programmu, kur lietotājs ievada gar faila nosaukumu un faila formātu (paplašinājumu),
+ un atkarībā no faila paplašinājuma tiek nolasīts faila saturs. 
+ Nolasītā informācija ir jāizdrukā terminālī. 
+ Ievērot iespejamās kļūdas!
+"""
---
+++ 5udz_terminal.py
@@ -0,0 +1,6 @@
+"""
+Izveidot Python programmu, kas ļauj lietotājam ievadīt savu vārdu terminālī. 
+Ievadīto vārdu pēc tam ierakstīt teksta failā (piemēram, "lietotajs.txt"). 
+Programmai jābūt spējīgai apstrādāt kļūdas, piemēram, faila nepieejamību vai rakstīšanas problēmas. 
+Pēc ierakstīšanas izvadīt paziņojumu par veiksmīgu darbību vai kļūdu gadījumā attiecīgu kļūdas ziņojumu.
+"""
