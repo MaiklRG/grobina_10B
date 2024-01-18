a3b86a62f5cb0967998bfd2d529a2356d3d69a96
MaiklRG <maikls.reinsons-guza@grobinasskola.lv>
2024-01-18 09:30:43 +0200

    Uzdevums

--- 1uzd_izdruk.py
+++ 1uzd_izdruk.py
@@ -1,1 +1,15 @@
 # Izveidot Python programmu, kas nolasītu un izdrukātu visu teksta faila saturu (txt formātā)
+
+def lasit():
+    try:
+        with open('parbaude.txt', "r", encoding="utf8") as datne:
+            for x in datne:
+                print(x)
+
+    except FileNotFoundError:
+        print("Datne nav atrasta failā!")
+
+
+
+if __name__=="__main__":
+    lasit()
