f9c102234f6a31c7c8dc10945138acea76fcba46
MaiklRG <maikls.reinsons-guza@grobinasskola.lv>
2024-01-18 09:53:05 +0200

    3.udz

--- 3uzd_rinda.py
+++ 3uzd_rinda.py
@@ -1,1 +1,15 @@
 # Izveidot Python programmu, kas nolasītu un izdrukātu trešās teksta faila rindas saturu
+
+def lasit_rinda():
+    try:
+        with open('parbaude.txt',"r", encoding="utf8") as datne:
+            for x in datne:
+                print(datne.readline())
+
+
+
+    except FileNotFoundError:
+        print("Nav atrasta datne fails!")
+
+if __name__=="__main__":
+    lasit_rinda()
